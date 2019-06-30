5a868b75fe15a5971ddad287b4c7d17d750acccc
Alan Klein <aklein@osisecure.com>
2019-06-30 15:00:05 -0400

    Added Different Firestore DB Examples and Comments

--- js/addTask.js
+++ js/addTask.js
@@ -39,14 +39,73 @@ submitBtn.addEventListener("click", function(e) {
     });
 });
 
+// Example on displaying all records in DB
+
 document.addEventListener("DOMContentLoaded", event => {
+  displayAllDBRecords();
+  displayFilteredDBRecords("washDishes");
+  updateSpecificRecord("X7iIXeEjIQhVF8BYaqwM");
+});
+
+// Example on displaying all records in DB from Cloud Firestore
+// Section: Get all documents in a collection
+// https://firebase.google.com/docs/firestore/query-data/get-data
+
+function displayAllDBRecords() {
   const db = firebase.firestore();
   db.collection("DigitalCrafts")
     .get()
     .then(function(querySnapshot) {
+      console.log("Display all Records!");
       querySnapshot.forEach(function(doc) {
         // doc.data() is never undefined for query doc snapshots
         console.log(doc.id, " => ", doc.data());
       });
     });
-});
+}
+
+// Example on displaying filtered records from DB from Cloud Firestore
+// Section: Get multiple documents from a collection
+// https://firebase.google.com/docs/firestore/query-data/get-data
+
+function displayFilteredDBRecords(filterTask) {
+  const db = firebase.firestore();
+  let query = db
+    .collection("DigitalCrafts")
+    .where("task", "==", filterTask)
+    .get()
+    .then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+        // doc.data() is never undefined for query doc snapshots
+        console.log("Display Filtered Records");
+        console.log(doc.id, " => ", doc.data());
+      });
+    })
+    .catch(function(error) {
+      console.log(`Error getting documents ${error}`);
+    });
+}
+
+// Example of updating a Firestore Document
+// Section: Update a document
+// https://firebase.google.com/docs/firestore/manage-data/add-data
+
+function updateSpecificRecord(documentID) {
+  const db = firebase.firestore();
+  let fireStoreCollection = db.collection("DigitalCrafts");
+  let documentToUpdate = fireStoreCollection.doc(documentID);
+  console.log(`Updating Record ID: ${documentID}`);
+  // Update the "accepted" and "acceptedUsername" fields
+  return documentToUpdate
+    .update({
+      accepted: true,
+      acceptedUsername: "bsmith"
+    })
+    .then(function() {
+      console.log("Document successfully updated!");
+    })
+    .catch(function(error) {
+      // The document probably doesn't exist.
+      console.error("Error updating document: ", error);
+    });
+}
